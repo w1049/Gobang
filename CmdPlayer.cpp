c8b70cd976928c221fdee4a30e98748e7c94edf7
w1049 <w10493@sjtu.edu.cn>
2021-12-10 12:14:26 +0800

    modify input

--- CmdPlayer.cpp
+++ CmdPlayer.cpp
@@ -18,8 +18,14 @@ ChessPiece CmdPlayer::getNextPos(const ChessPad* pad) {
         cout << "轮到" << (pid == 1 ? "●" : "○") << "方下棋。请输入坐标，形式为“A8”\n（不含引号，若格式不对我也不知道会发生什么）: " << endl;
         char cy;
         int x;
-        cin >> cy >> x;
-        if (reason = pad->check(p = ChessPiece(pid, x-1, cy-'A'))) {
+        cin.clear(); cin.sync();
+        cin >> cy;
+        if ('a' <= cy && cy <= 'z') cy -= 'a';
+        else if ('A' <= cy && cy <= 'Z') cy -= 'A';
+        else continue;
+        cin >> x;
+        if (cin.fail() || x < 1 || x > 15 || cy < 0 || cy >= 15) continue;
+        if (reason = pad->check(p = ChessPiece(pid, x-1, cy))) {
             infoFailed(p, reason);
         }
         else return p;
@@ -27,5 +33,5 @@ ChessPiece CmdPlayer::getNextPos(const ChessPad* pad) {
 }
 
 void CmdPlayer::infoFailed(ChessPiece p, uint8_t reason) {
-    cout << "由于原因" << (int)reason << ", 你不能在(" << (int)p.getPosX() << "," << (int)p.getPosY() << ")下棋." << endl;
+    cout << "由于原因" << (int)reason << ", 你不能在" << char(p.getPosY() + 'A') << (int)p.getPosX() + 1 << "下棋." << endl;
 }
